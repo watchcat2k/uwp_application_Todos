7bc5a34d891407d41e4eb4c96e66a15eb8961b05
WatchCat <810549046@qq.com>
2018-03-27 16:47:41 +0800

    uwp project Todos week2 process

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace Todos
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void checkBox1Click(object sender, RoutedEventArgs e)
+        {
+            if (line1.Visibility == Visibility.Visible)
+            {
+                line1.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                line1.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void navToButton(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(NewPage));
+        }
+    }
+}
---
+++ NewPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Todos
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class NewPage : Page
+    {
+        public NewPage()
+        {
+            this.InitializeComponent();
+        }
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            textTitle.Text = "";
+            textDetail.Text = "";
+            DueDate.Date = DateTime.Now;
+        }
+
+        private async void CreateClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new ContentDialog()
+            {
+                Title = "提示信息",
+                PrimaryButtonText = "确定"
+            };
+
+            if (textTitle.Text == "")
+            {
+                dialog.Content = "标题不能为空";
+                await dialog.ShowAsync();
+            }
+            else if (textDetail.Text == "")
+            {
+                dialog.Content = "内容主体不能为空";
+                await dialog.ShowAsync();
+            }
+            else if (DueDate.Date < DateTime.Now)
+            {
+                dialog.Content = "日期不正确";
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                dialog.Content = "创建成功";
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
