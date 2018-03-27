c497d878f2e14f4face1991536fbebf6b0fb3a75
WatchCat <810549046@qq.com>
2018-03-27 17:01:50 +0800

    add models and ViewModels according to the ppt

---
+++ Models/TodoItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todos.Models
+{
+    class TodoItem
+    {
+        private string id;
+        public string title { get; set; }
+        public string description { get; set; }
+        public bool completed { get; set; }
+        public DateTime duedate { get; set; }
+        //public string coverImage { get; set; }
+
+        public TodoItem(string title, string description, DateTime duedate)
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.title = title;
+            this.description = description;
+            this.completed = false;
+            this.duedate = duedate;
+        }
+    }
+}
---
+++ ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Todos.ViewModels
+{
+    class TodoItemViewModel
+    {
+        private ObservableCollection<Models.TodoItem> allItems = new ObservableCollection<Models.TodoItem>();
+        public ObservableCollection<Models.TodoItem> AllItems { get { return this.allItems; } }
+
+        public void AddTodoItem(string title, string description, DateTime duedate)
+        {
+            this.allItems.Add(new Models.TodoItem(title, description, duedate));
+        }
+
+        public void RemoveTodoItem(string id)
+        {
+            //this.selectedItem = null;
+        }
+
+        public void UpdateTodoItem(string id, string title, string description)
+        {
+            //this.selectedItem = null;
+        }
+    }
+}
